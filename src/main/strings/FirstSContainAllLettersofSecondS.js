87d35eeee424eb11afbf7389776212e568ab9577
Kgresmer <kgresmer@gmail.com>
2017-06-19 11:02:45 -0700

    Does the first string contain all the letters of the second string. solution 2

--- src/main/strings/FirstSContainAllLettersofSecondS.js
+++ src/main/strings/FirstSContainAllLettersofSecondS.js
@@ -14,3 +14,18 @@ function mutation(arr) {
 }
 
 mutation(["hello", "hey"]);
+
+function isBigEnough(element, index, array) {
+    return element >= 10;
+}
+[12, 5, 8, 130, 44].every(isBigEnough);   // false
+[12, 54, 18, 130, 44].every(isBigEnough);
+
+function mutationInterSol(arr) {
+    return arr[1].toLowerCase()
+        .split('')
+        .every(function(letter) {
+            return arr[0].toLowerCase()
+                    .indexOf(letter) != -1;
+        });
+}
